e386f5b7831ac8cc4a769b1e51f5ba59d6c9686c
Vinutha2608 <vinuthaacharya26@gmail>
2026-05-15 18:07:03 +0530

    implemented secrets

--- utils/config_reader.py
+++ utils/config_reader.py
@@ -2,11 +2,11 @@ import configparser
 import os
 
 config = configparser.ConfigParser()
-
-# Get full path of config file
 file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config", "config.ini")
-
 config.read(file_path)
 
 def get_config(key):
+    if key == "url":
+        # In CI reads from GitHub Secret, locally reads from config.ini
+        return os.getenv("SAUCE_URL", config['DEFAULT'][key])
     return config['DEFAULT'][key]
